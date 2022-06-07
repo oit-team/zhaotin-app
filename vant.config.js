6257589ae487b336efb997dc33f94b44471bbbcf
inlinger <inlinger@outlook.com>
2022-06-07 15:11:57 +0800

    chore: 配置环境、vant变量

---
+++ vant.config.js
@@ -0,0 +1,8 @@
+const theme = require('./src/theme')
+
+module.exports = {
+  'button-primary-background-color': theme.colors.primary,
+  'button-primary-border-color': theme.colors.primary,
+  'button-info-background-color': theme.colors.black,
+  'button-info-border-color': theme.colors.black,
+}
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
+const modifyVars = require('./vant.config')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
+  devServer: {
+    port: process.env.PORT || 5000,
+    proxy: {
+      '/api': {
+        target: process.env.VUE_APP_API_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
+        },
+      },
+    },
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars,
+      },
+    },
+  },
 })
