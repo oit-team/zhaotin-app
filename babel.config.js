6ae4dd28e9df4af4c06f13c8832b265dc11b6b19
inlinger <inlinger@outlook.com>
2022-06-17 18:54:09 +0800

    build: 添加可选链和空值合并babel插件

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,8 @@ module.exports = {
     '@vue/cli-plugin-babel/preset',
     '@vue/babel-preset-jsx',
   ],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+  ],
 }
